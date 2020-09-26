64cd312753c348b94233747df1c011dd82bbef39
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-09-26 14:37:01 -0400

    Creando el contexto y Models

---
+++ PrestamoTarea/Models/Personas.cs
@@ -0,0 +1,26 @@
+﻿using PrestamoTarea.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoTarea.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Nombres { get; set; }
+
+        [CedulaValidacion]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+
+        public double Balance { get; set; }
+    }
+}
---
+++ PrestamoTarea/Models/Prestamos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrestamoTarea.Models
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public string Concepto { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public double Monto { get; set; }
+
+        public double Balance { get; set; }
+    }
+}
